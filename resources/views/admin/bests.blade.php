176f25ba22aada2961a59302023c020b04900733
Sajjad Rahnama <sajjad.rahnama7@gmail.com>
2017-06-15 18:30:16 +0430

    Bests Ranking

--- resources/views/admin/bests.blade.php
+++ resources/views/admin/bests.blade.php
@@ -21,16 +21,32 @@
                     </div>
                 </div>
                 <div class="row">
-                    <div class="col-md-8 col-md-offset-2 form-dialog" style="background-color:rgba(255,255,255,.7);">
+                    <div class="col-md-10 col-md-offset-1 form-dialog" style="background-color:rgba(255,255,255,.7);">
                         <div class="bests-from">
                             <h3 class="text-muted">{{'ترین های دانشکده'}}</h3>
+                            <div class="row">
+                                <div class="col-xs-6 col-sm-3">
+                                    <h4>{{'نفر سوم'}}</h4>
+                                </div>
+                                <div class="col-xs-6 col-sm-3">
+                                    <h4>{{'نفر دوم'}}</h4>
+                                </div>
+                                <div class="col-xs-6 col-sm-3">
+                                    <h4>{{'نفر اول'}}</h4>
+                                </div>
+                                <div class="col-xs-6 col-sm-3">
+                                    <h4>{{'عنوان ترین'}}</h4>
+                                </div>
+
+
+                            </div>
                             @foreach($answers as $key => $q)
                                 <div class="row">
-                                    <div class="hidden-xs col-sm-2 pull-right"></div>
-                                    <div class="form-group col-xs-6 col-sm-4 pull-right">
+
+                                    <div class="form-group col-xs-6 col-sm-3 pull-right">
                                         <h4>{{$titles[$key]}}</h4>
                                     </div>
-                                    <div class="form-group col-xs-6 col-sm-4 pull-right">
+                                    <div class="form-group col-xs-6 col-sm-3 pull-right">
                                         <h5>
                                             @if(count($q))
                                                 @if($q[0]['num']>=10)
@@ -41,7 +57,30 @@
                                                 {{id_to_name($q[0]['id'])}}
                                             @endif
                                         </h5>
-
+                                    </div>
+                                    <div class="form-group col-xs-6 col-sm-3 pull-right">
+                                        <h5>
+                                            @if(count($q)>1)
+                                                @if($q[1]['num']>=10)
+                                                    <span class="label label-success">{{$q[1]['num']}}</span>
+                                                @else
+                                                    <span class="label label-info">{{$q[1]['num']}}</span>
+                                                @endif
+                                                {{id_to_name($q[1]['id'])}}
+                                            @endif
+                                        </h5>
+                                    </div>
+                                    <div class="form-group col-xs-6 col-sm-3 pull-right">
+                                        <h5>
+                                            @if(count($q)>2)
+                                                @if($q[2]['num']>=10)
+                                                    <span class="label label-success">{{$q[2]['num']}}</span>
+                                                @else
+                                                    <span class="label label-info">{{$q[2]['num']}}</span>
+                                                @endif
+                                                {{id_to_name($q[2]['id'])}}
+                                            @endif
+                                        </h5>
                                     </div>
                                 </div>
                             @endforeach
